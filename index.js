fff0d5ff77ee4db077b577324454c8688b8e410b
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-04-23 22:53:09 +0600

    added method to pull multiple data from mongoDB database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -11,7 +12,38 @@ app.use(cors());
 app.use(express.json());
 
 //
+// eslint-disable-next-line prettier/prettier
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1-km.7tchu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
 
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('car-services').collection('service');
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/service/:id', async (req, res) => {
+            const { id } = req.params;
+            const query = { _id: ObjectId(id) };
+        });
+    } finally {
+        // nothing here!
+    }
+}
+
+run().catch(console.dir);
+
+// Get request
 app.get('/', (req, res) => {
     res.send('Running car service server.');
 });
